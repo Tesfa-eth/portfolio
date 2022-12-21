d851dac464f6f7fb3bed870eac04f3c760599359
Tesfatsion Shiferaw <62855279+Tesfa-eth@users.noreply.github.com>
2022-12-20 23:47:03 -0500

    commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1ba1acc10cfccd51"; }
+/******/ 		__webpack_require__.h = function() { return "303c9b58bee7d51a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/portfolioCard.js
+++ pages/portfolioCard.js
@@ -4,8 +4,18 @@ import Image from "next/legacy/image";
 export default function PortfolioCard({image}) {
   return (
     <div className='basis-1/3 flex-1 bg-slate-600 rounded-lg '>
-        
+        <div class="relative bg-black">
         <Image src={image} className='object-cover rounded-lg' width={'100%'} height={'100%'} layout='responsive'/>
+        <ul>
+            <li className='absolute bottom-14 left-16 h-8 w-44 bg-gradient-to-r from-cyan-500 to-teal-800 text-center rounded md:bottom-20 md:left-36 md:h-16 md:w-80 md:pt-2 hover:to-white'>
+                <a className=' text-white rounded md:text-3xl' href='#'>Live Demo</a>
+            </li>
+            <li className='absolute bottom-3 left-16 h-8 w-44 bg-gradient-to-r from-cyan-500 to-teal-800 text-center rounded md:bottom-2 md:left-36 md:h-16 md:w-80 md:pt-2'>
+                <a className=' text-white rounded md:text-3xl' href='#'>Source code</a>
+            </li>
+        </ul>
+        
+        </div>
         <div className='flex text-center flex-col items-center'>
             <h3 className=''>Project name</h3>
             <h5 className=''>Project description</h5>
